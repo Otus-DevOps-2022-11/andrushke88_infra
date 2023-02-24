57ffcb43c549514eeb7da33beb96d3f915226284
Andrey Kopyltsov <andrushke@gmail.com>
2023-02-24 13:35:15 +0600

    HW4-5

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -3,7 +3,7 @@ wget -qO - "https://www.mongodb.org/static/pgp/server-4.2.asc" | sudo apt-key ad
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb-org mongodb
 sleep 30
 ps --no-headers -o comm 1
 sudo systemctl daemon-reload
