abb069435f0c67d590d4026f5071f5ebdcb8f150
Andrey Kopyltsov <andrushke@gmail.com>
2023-02-24 13:56:48 +0600

    HW4-10

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+#!/usr/bin/env bash
+sudo apt update
+echo "sleep 30 sec for install updates"; sleep 30;
+sudo apt-get install apt-transport-https ca-certificates
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+echo "sleep 30 sec for install updates"; sleep 30;
+sudo apt install -y mongodb-org
+echo "Check mongod status"
 sudo systemctl status mongod
+echo "Start mongod"
+sudo systemctl start mongod
+echo "Check mongod status again"
+sudo systemctl status mongod
+echo "Add mongod to autostart"
+sudo systemctl enable mongod
+
+journalctl -xe
