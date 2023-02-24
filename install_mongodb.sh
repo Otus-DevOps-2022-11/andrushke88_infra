66054cbb8d3a5f9dc260f9476b3fa0276b8b5b0f
Andrey Kopyltsov <andrushke@gmail.com>
2023-02-24 09:53:10 +0600

    HW4 more sleep 4 king of sleep

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 sudo apt-get update
+sleep 30
 sudo apt-get install apt-transport-https ca-certificates
 wget -qO - "https://www.mongodb.org/static/pgp/server-4.2.asc" | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
+sleep 30;
 sudo apt-get install -y mongodb-org
-sleep 50
-sudo /etc/init.d/dbus start
+sleep 30
+sudo systemctl status mongod
 sudo systemctl enable mongod
+sudo systemctl status mongod
 sudo systemctl start mongod
